659571ed771732b8b08e1f549834f871fb5be7ef
dstroppolo <stroppolo.daniel@gmail.com>
2019-02-07 02:24:38 -0500

    added unit test for get body message method

---
+++ app/ui/src/test/java/com/fsck/k9/view/MessageContainerTest.java
@@ -0,0 +1,35 @@
+package com.fsck.k9.view;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.fsck.k9.ui.messageview.MessageContainerView;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+
+
+public class MessageContainerTest {
+
+    private String emailHtml = "<html><head></head><body><meta name=\"viewport\" content=\"width=device-width><style type=\"text/css\">" +
+            " pre.k9mail {white-space: pre-wrap; word-wrap:break-word; font-family: sans-serif; margin-top: 0px}</style>\n" +
+            "      <div dir=\"ltr\">this is a test email to see whats up. what do you think?</div></body></html>";
+
+    private String expectedBody = "this is a test email to see whats up. what do you think?";
+
+    @Test
+    public void checkCorrectBody() {
+
+        Context context = mock(Context.class);
+        AttributeSet attributeSet = mock(AttributeSet.class);
+
+        MessageContainerView msv = new MessageContainerView(context, attributeSet);
+        String actualBody = msv.getMessageText(emailHtml);
+
+        assertEquals("expectedBody", "actualBody");
+    }
+
+}
