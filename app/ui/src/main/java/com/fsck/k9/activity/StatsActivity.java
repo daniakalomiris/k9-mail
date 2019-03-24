a3a6a6e88344cfb17d01c23995f874c235201bde
Dania Kalomiris <daniakalomiris@gmail.com>
2019-03-24 17:48:55 -0400

    Get all inbox messages from local storage #20

--- app/ui/src/main/java/com/fsck/k9/activity/StatsActivity.java
+++ app/ui/src/main/java/com/fsck/k9/activity/StatsActivity.java
@@ -7,13 +7,16 @@ import android.view.View;
 
 import com.fsck.k9.Account;
 import com.fsck.k9.Preferences;
-import com.fsck.k9.controller.MessagingController;
 import com.fsck.k9.mail.MessageRetrievalListener;
-import com.fsck.k9.mailstore.LocalFolder;
+import com.fsck.k9.mail.MessagingException;
 import com.fsck.k9.mailstore.LocalMessage;
-import com.fsck.k9.mailstore.LocalStore;
-import com.fsck.k9.mailstore.LocalStoreProvider;
 import com.fsck.k9.ui.R;
+import com.fsck.k9.mailstore.LocalStoreProvider;
+import com.fsck.k9.mailstore.LocalStore;
+import com.fsck.k9.mailstore.LocalFolder;
+import com.fsck.k9.DI;
+
+import java.util.List;
 
 public class StatsActivity extends K9Activity implements View.OnClickListener {
 
@@ -21,16 +24,17 @@ public class StatsActivity extends K9Activity implements View.OnClickListener {
     String userId;
     LocalStore localStore;
     LocalFolder localFolder;
+    List<LocalMessage> messages;
     private Account account = Preferences.getPreferences(this).getDefaultAccount();
-
+    MessageRetrievalListener<LocalMessage> listener;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setLayout(R.layout.activity_stats);
-        String blah = account.getLocalStorageProviderId();
-        String fkljsd = account.getInboxFolder();
+        String localStorageProvider = account.getLocalStorageProviderId();
+        String inbox = account.getInboxFolder();
         LocalStoreProvider localStoreProvider = new LocalStoreProvider();
         int i = 0;
         try {
@@ -38,17 +42,20 @@ public class StatsActivity extends K9Activity implements View.OnClickListener {
             //localStore.searchForMessages();
             localFolder = localStore.getFolder("INBOX");
             i = localFolder.getMessageCount();
+            messages = localFolder.getMessages(listener);
         } catch (Exception e) {
-            // Print exception
+            e.printStackTrace();
         }
-        System.out.println(i);
+        System.out.println("Inbox Message Count: " + i);
 
-
+        System.out.println("All Inbox Messages in Local Storage");
+        for (LocalMessage message : messages) {
+            System.out.println(message);
+        }
     }
 
     @Override
     public void onClick(View v) {
-
     }
 
     public static void launch(Activity activity){
