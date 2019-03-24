4452392f44850d6537e277b2cc69a4e18931a3fc
Jeff Buonamici <jeffbuonamici@users.noreply.github.com>
2019-03-24 16:54:35 -0400

    Getting Stats from local storage #20

--- app/ui/src/main/java/com/fsck/k9/activity/StatsActivity.java
+++ app/ui/src/main/java/com/fsck/k9/activity/StatsActivity.java
@@ -5,11 +5,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.fsck.k9.Account;
+import com.fsck.k9.Preferences;
+import com.fsck.k9.controller.MessagingController;
+import com.fsck.k9.mail.MessageRetrievalListener;
+import com.fsck.k9.mailstore.LocalFolder;
+import com.fsck.k9.mailstore.LocalMessage;
+import com.fsck.k9.mailstore.LocalStore;
+import com.fsck.k9.mailstore.LocalStoreProvider;
 import com.fsck.k9.ui.R;
 
 public class StatsActivity extends K9Activity implements View.OnClickListener {
 
-
+    String serverId;
+    String userId;
+    LocalStore localStore;
+    LocalFolder localFolder;
+    private Account account = Preferences.getPreferences(this).getDefaultAccount();
 
 
     @Override
@@ -17,6 +29,19 @@ public class StatsActivity extends K9Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
 
         setLayout(R.layout.activity_stats);
+        String blah = account.getLocalStorageProviderId();
+        String fkljsd = account.getInboxFolder();
+        LocalStoreProvider localStoreProvider = new LocalStoreProvider();
+        int i = 0;
+        try {
+            localStore = localStoreProvider.getInstance(account);
+            //localStore.searchForMessages();
+            localFolder = localStore.getFolder("INBOX");
+            i = localFolder.getMessageCount();
+        } catch (Exception e) {
+            // Print exception
+        }
+        System.out.println(i);
 
 
     }
