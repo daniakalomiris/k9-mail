d73b8897b7335a9dc8b73bd00b1fed3f7b71cbd7
MrWorme <micu.p@outlook.com>
2019-03-24 22:57:51 -0400

    Graphes for stats
    Graph was added to display what day of the week the person gets the most emails. information is persistent and the more local storage accumulates info, the more it populates the graph #20 ,#39

--- app/ui/src/main/java/com/fsck/k9/activity/StatsActivity.java
+++ app/ui/src/main/java/com/fsck/k9/activity/StatsActivity.java
@@ -2,8 +2,11 @@ package com.fsck.k9.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.fsck.k9.Account;
 import com.fsck.k9.Preferences;
@@ -14,6 +17,15 @@ import com.fsck.k9.ui.R;
 import com.fsck.k9.mailstore.LocalStoreProvider;
 import com.fsck.k9.mailstore.LocalStore;
 import com.fsck.k9.mailstore.LocalFolder;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.*;
 
@@ -30,41 +42,105 @@ public class StatsActivity extends K9Activity implements View.OnClickListener {
     private int messagesLastWeek = 0;
     private static Hashtable<Address[], Integer> mSenders = new Hashtable<Address[], Integer>();
 
+
+    BarChart barChart;
+    ArrayList<String> days;
+    ArrayList<BarEntry> barEntries;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setLayout(R.layout.activity_stats);
-        
+
         try {
             LocalStoreProvider localStoreProvider = new LocalStoreProvider();
             localStore = localStoreProvider.getInstance(account);
             localFolder = localStore.getFolder("INBOX");
-            messages = localFolder.getMessages(listener);
+            //messages = localFolder.getMessages(listener);
+            List<String> allMessages =localFolder.getAllMessageUids();
+            messages = localFolder.getMessagesByUids(allMessages);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        if(messages != null)
+        barChart = (BarChart) findViewById(R.id.barGraph);
+        if (messages != null) {
             crunchData();
-            senderStats();
+            createDateBarChart();
+        }
+        senderStats();
+        SetMostFrequentSender();
+    }
+
+    private void SetMostFrequentSender() {
+        TextView senderText = findViewById(R.id.senderID);
+
+        senderText.setText(getMostFrequentSender());
+    }
+
+    private void createDateBarChart() {
+        days = new ArrayList<>();
+        days.add(0,"Sun");
+        days.add(1,"Mon");
+        days.add(2,"Tue");
+        days.add(3,"Wed");
+        days.add(4,"Thu");
+        days.add(5,"Fri");
+        days.add(6,"Sat");
+        barEntries = new ArrayList<>();
+        barEntries.add(new BarEntry(0,dayOfWeek.get("Sun")));
+        barEntries.add(new BarEntry(1,dayOfWeek.get("Mon")));
+        barEntries.add(new BarEntry(2,dayOfWeek.get("Tue")));
+        barEntries.add(new BarEntry(3,dayOfWeek.get("Wed")));
+        barEntries.add(new BarEntry(4,dayOfWeek.get("Thu")));
+        barEntries.add(new BarEntry(5,dayOfWeek.get("Fri")));
+        barEntries.add(new BarEntry(6,dayOfWeek.get("Sat")));
+
+
+        XAxis xAxis = barChart.getXAxis();
+
+        xAxis.setValueFormatter(new MyXAxisValueFormatter(days));
+
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        xAxis.setDrawAxisLine(false);
+        xAxis.setDrawGridLines(false);
+
+        BarDataSet bds = new BarDataSet(barEntries, "Days");
+        bds.setColors(ColorTemplate.PASTEL_COLORS);
+
+        BarData theData = new BarData(bds);
+        barChart.setTouchEnabled(false);
+        barChart.getDescription().setEnabled(false);
+        barChart.getLegend().setEnabled(false);
+        barChart.setData(theData);
+        barChart.setFitBars(true);
+
+
+
     }
 
     private void crunchData() {
-        for(LocalMessage m : messages) {
+        dayOfWeek.put("Sun", 0);
+        dayOfWeek.put("Mon", 0);
+        dayOfWeek.put("Tue", 0);
+        dayOfWeek.put("Wed", 0);
+        dayOfWeek.put("Thu", 0);
+        dayOfWeek.put("Fri", 0);
+        dayOfWeek.put("Sat", 0);
+        for (LocalMessage m : messages) {
             // Calculate day frequency
-            String day = (m.getSentDate()).toString().substring(0,3);
-            if(!dayOfWeek.containsKey(day))
-                dayOfWeek.put(day, 1);
-            else
-                dayOfWeek.put(day, dayOfWeek.get(day) + 1);
+            String day = (m.getSentDate()).toString().substring(0, 3);
+
+            dayOfWeek.put(day, dayOfWeek.get(day) + 1);
 
             Calendar cal = new GregorianCalendar();
             cal.add(Calendar.DAY_OF_MONTH, -7);
             Date sevenDaysAgo = cal.getTime();
 
             // Messages sent within the last week
-            if(m.getSentDate().before(sevenDaysAgo))
+            if (m.getSentDate().before(sevenDaysAgo))
                 messagesLastWeek++;
         }
     }
@@ -80,24 +156,26 @@ public class StatsActivity extends K9Activity implements View.OnClickListener {
     }
 
     // Get the most frequent sender from all local messages
-    private Address[] getMostFrequentSender() {
+    private String getMostFrequentSender() {
         int maxSender = Collections.max(mSenders.values());
 
         Address[] mostFrequentSender = null;
         Enumeration senders;
         Address[] key;
         senders = mSenders.keys();
-        while(senders.hasMoreElements()) {
+        while (senders.hasMoreElements()) {
             key = (Address[]) senders.nextElement();
             if (mSenders.get(key) == maxSender) {
                 mostFrequentSender = key;
             }
+            Log.i("key", "getMostFrequentSender: "+mSenders.get(key));
         }
-        return mostFrequentSender;
+        return null;
+       // return mostFrequentSender[0].getAddress();
     }
 
     private void senderStats() {
-        for(LocalMessage m : messages) {
+        for (LocalMessage m : messages) {
             addSender(m.getSender());
         }
         getMostFrequentSender();
@@ -108,8 +186,27 @@ public class StatsActivity extends K9Activity implements View.OnClickListener {
     public void onClick(View v) {
     }
 
-    public static void launch(Activity activity){
+    public static void launch(Activity activity) {
         Intent intent = new Intent(activity, StatsActivity.class);
         activity.startActivity(intent);
     }
+
+    private class MyXAxisValueFormatter   extends ValueFormatter
+    {
+        private ArrayList<String> dayString;
+
+        public MyXAxisValueFormatter (ArrayList<String> days) {
+            dayString = days;
+        }
+
+        @Override
+        public String getFormattedValue(float value) {
+            Log.i("value", "getFormattedValue: "+value);
+            return dayString.get((int) value);
+        }
+
+
+    }
+
+
 }
