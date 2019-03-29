887d6b1f061432fa12a6f1c2702efa5210c5e6d0
MrWorme <micu.p@outlook.com>
2019-03-28 21:27:34 -0400

    email templates xml
    The content of the templates themselves are found in a new xml file named templates in the ui/res/values folder #22 #61

--- app/ui/src/main/java/com/fsck/k9/activity/MessageCompose.java
+++ app/ui/src/main/java/com/fsck/k9/activity/MessageCompose.java
@@ -467,12 +467,9 @@ public class MessageCompose extends K9Activity implements OnClickListener,
         }
 
         Spinner spinner = (Spinner) findViewById(R.id.template);
-// Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.template_array, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.template_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
+
         spinner.setAdapter(adapter);
 
 
